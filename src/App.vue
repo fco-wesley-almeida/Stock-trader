b13fe6aa5baf67c1e2319500bc67290b026edc50
almeidafpw <fco.wesley.almeida@gmail.com>
2020-04-15 18:59:36 -0300

    Rotas configuradas.

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,20 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <router-view></router-view>
     </div>
-    <router-view/>
   </div>
 </template>
+
+<script>
+
+// import Home from '@/views/Home'
+
+// export default {
+//   components: { Home }
+// }
+
+</script>
 
 <style>
 #app {
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,15 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <h1>Inicio</h1>
+    <router-link to="/Stocks" tag="div" active-class="active" exact>
+      <a>Ações</a>
+    </router-link>
+
+    <router-link to="/Portfolio" tag="div" active-class="active" exact>
+      <a>Portfolio</a>
+    </router-link>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
-export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
 </script>
